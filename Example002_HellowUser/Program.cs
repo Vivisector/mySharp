6ccb64a2be4ae959f05cf2ef9ae277d0f5c9b4c5
Dmitry Beliakov <hulk295@yandex.ru>
2022-11-27 21:13:36 +0300

    Example002_HellowUser - добавили ввод имени пользователя

---
+++ Example002_HellowUser/Program.cs
@@ -0,0 +1,15 @@
+﻿//string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+
+//ver2.0
+//Console.WriteLine("Представьтесь уже");
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, " + username + "!");
+
+//ver2.1 -- выписка в одну строку только командой, а не плюсами
+Console.Write("Представьтесь уже ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+Console.Write("!");
