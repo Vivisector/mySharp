a8e8677559a17cc34e29e2c7a227b17b8319cd21
Dmitry Beliakov <hulk295@yandex.ru>
2022-11-28 16:06:15 +0300

    Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿//int numA = 12;
+double numA = 12;
+//MyClass2 numberB = null;
+double numB = 5; //double - тип переменной, позволяющей работать с вещественными числами
+Console.WriteLine(numA / numB);
