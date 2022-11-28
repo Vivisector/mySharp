4e04bf2002755c0b109dc0f17b390f61337bcf77
Dmitry Beliakov <hulk295@yandex.ru>
2022-11-28 21:38:50 +0300

    записали прграммным языком задачку по выбору самой тяжелой гири при помощи простого условия if then

---
+++ Example006_Giri/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 9;
+int c = 3;
+int d = 5;
+int e = 2;
+int f = 4;
+int g = 6;
+int h = 7;
+int mMax = a;
+if (b>mMax){mMax=b;};
+if (c>mMax){mMax=c;};
+if (d>mMax){mMax=d;};
+if (e>mMax){mMax=e;};
+if (f>mMax){mMax=f;};
+if (g>mMax){mMax=g;};
+if (h>mMax){mMax=h;};
+
+Console.WriteLine("Максимальное значение среди представленных чисел: " + mMax);
