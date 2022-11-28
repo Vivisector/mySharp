66b47c822c0708db88a1740dcdf909512b5009b6
Dmitry Beliakov <hulk295@yandex.ru>
2022-11-28 21:26:57 +0300

    проверили условие if else + привели к нижнему регистру буквы в строке командой ToLower()

---
+++ Example005_Dating/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+блок схема:
+НачалО
+ввести имя пользоателя
+ЕСЛИ имя Маша, то особенное приветствие
+ИНАЧЕ обычное приветствие
+КонеЦ
+*/
+Console.WriteLine("Представьтесь, плз:");
+string mUser = Console.ReadLine();
+if (mUser.ToLower() == "маша"){
+Console.WriteLine("Привет, Маша, давно не виделись!");
+}else{
+Console.WriteLine("Здравствуйте, " + mUser + "!");    
+}
+//Console.WriteLine("Hello, World!");
