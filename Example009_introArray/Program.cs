52f611e8aaf41c45eda1d2e17cfaa293471e35d0
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-01 22:40:27 +0300

    потренировали перебор элементов массива

---
+++ Example009_introArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int[] arr={18,21,32,42,15,26,47,18,29};
+int n = arr.Length;
+int find = 48;
+int index = -1;
+
+while(index < n)
+{
+    if (find == arr[index]) {
+        Console.WriteLine("Индекс искомогоэлемента: " + index); break;
+    }
+    index++;
+}
