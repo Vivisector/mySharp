f1c4635e51e6c16352238cfcf4819b32c8d3aae5
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-03 17:40:48 +0300

    разобрались с минусом для фиксации НЕнайденного элемента

---
+++ My_index/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = { 18, 21, 32, 42, 15, 26, 47, 18, 29 };
+int find = 20;
+
+// int Max(int arg1, int arg2, int arg3)
+
+int mFind(int[] arr, int find) // определили функцию
+{
+    int index = 0;
+    int n = arr.Length;
+    int position = -1;
+    while (index < n)
+    {
+        if (arr[index] == find)
+        {
+            position = index;
+
+        }
+        index++;
+    }
+    return position;
+
+}
+
+if (mFind(arr, find) == -1)
+{
+    Console.WriteLine("Элемент " + Convert.ToString(find) + " не входит в представленный список");
+}
+else
+{
+    Console.WriteLine("Элемент " + Convert.ToString(find) + " находится на " + (mFind(arr, find) + 1) + " месте представленного списка");
+}
+
+// Console.WriteLine("Hello, World!");
--- My_voids/Program.cs
+++ My_voids/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿ // See https://aka.ms/new-console-template for more information
 // 1. Создаем пустой массив на 10 ячеек
 int[] arr = new int[1000]; // создали
 // теперь метод для заполнения
@@ -38,5 +38,25 @@ Printarr(arr);
 Console.WriteLine(""); //новая строка
 Console.WriteLine("Итого набралось: " + Summa(arr));
 
+int pos(int[] arr, int find)
+{
+    int i = 0;
+    int length = arr.Length;
+    int position = -1;
+    while(i<length){
+        if (arr[i]==find){
+            position = i;
+            break;
+        }
+    i++;
+    }
+    return position;
+}
+int mf = 18;
+int posit = pos(arr, mf);
+string sposit = Convert.ToString(posit);
+string sfind = Convert.ToString(mf);
+Console.WriteLine();
+Console.WriteLine("Элемент " + sfind + " находится на " + sposit + "-й позиции (первое вхождение)");
 // хорошо бы написать прогу, чтобы цифрами нарисовался бы портрет Ленина (напр.)
 // надо по исходной картинке темноту пикселя сопоставить цифрам
