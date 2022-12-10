0cc2e509347886e9551c66a88d26bb75810f44fa
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-10 12:49:10 +0300

    улучшение картинки вывода для Tablitsa_Umnozenia

--- Example011_Tablitsa_Umnozenia/Program.cs
+++ Example011_Tablitsa_Umnozenia/Program.cs
@@ -1,8 +1,9 @@
 ﻿// моделируем таблицу умножения, используя цикл в цикле
 // int[] a = {0,1,2,3,4,5,6,7,8,9};
 for (int i=1;i<10;i++){
-    for (int k = 1; k<10; k++){
+    for (int k = 1; k<=10; k++){
         Console.WriteLine($"{i} x {k} = {i*k}");
     }
+Console.WriteLine();
 }
 
